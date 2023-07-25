683f32052d5975242117fdb611f3ab339ca23793
epsilondelta <epsilonn.delta.co@gmail.com>
2023-07-25 20:10:49 +0900

    set jar file name

--- src/main/java/com/mysite/hellogradle/MainController.java
+++ src/main/java/com/mysite/hellogradle/MainController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
-    @GetMapping("/sbb")
+    @GetMapping("/")
     @ResponseBody
     public String index() {
-    	return "안녕하세요 sbb에 오신것을 환영합니다.";
+    	return "안녕하세요 Azure App service에 오신것을 환영합니다.\n 이 앱은 gradle로 빌드되었고 Azure App service에 배포되었습니다.";
     }
 }
